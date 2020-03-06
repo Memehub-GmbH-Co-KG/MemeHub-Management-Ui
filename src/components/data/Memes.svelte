55768fc279c444a2d861ff5fa624522f5cdafad9
bertye <baert.ye@gmail.com>
2020-03-06 19:01:15 +0100

    less button styling

--- src/components/data/Memes.svelte
+++ src/components/data/Memes.svelte
@@ -14,23 +14,8 @@
         margin-left: auto;
         margin-right: auto;
         display: block;
-        height: 3rem;
-        width: 8rem;
-        font-size: var(--font-size-l);
         background-color: var(--color-bg-a);
         color: var(--color-font-a);
-        border: none;
-        transform: none;
-        outline:none;
-    }
-    .load-more:hover {
-        cursor: pointer;
-        transform: none;
-        outline:none;
-    }
-    button.load-more:active, button.load-more:focus, button.load-more:selected {
-        transform: none;
-        outline:none;
     }
 </style>
 
